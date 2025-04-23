deb65c0ed5d32dd85141de0c2bcba3c6871f0ca4
Ana Carolina <acarolbrianez@hotmail.com>
2025-04-23 20:21:40 -0300

    Add files via upload

---
+++ exerc3aula11.c
@@ -0,0 +1,23 @@
+//exercicio 3 - aula 11:
+//3 - Crie um programa em C que recebe 4 notas, armazene as notas em um vetor. Some o total das notas usando for, guarde na variável total. Mostre o resultado com duas casas decimais.
+
+#include <stdio.h>
+#define NOTA 4 
+
+int main() {
+  float notas[NOTA];
+  float total = 0.0;
+
+  printf("Digite quatro notas:\n");
+  for (int i = 0; i < NOTA; i++) {
+    scanf("%f", &notas[i]);
+  }
+
+  for (int i = 0; i < NOTA; i++) {
+    total += notas[i];
+  }
+
+  printf("A soma total das notas é: %.2f\n", total);
+
+  return 0;
+}
